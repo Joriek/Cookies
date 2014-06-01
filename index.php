5c029bea226eb0138f96e42eba57fb860a30bba9
Joriek Rogge <Rogge.j@telenet.be>
2014-06-01 14:08:16 +0200

    cookie works, session works, delete is bugged

--- index.php
+++ index.php
@@ -1,41 +1,44 @@
 <?php 
-session_start();
+
 require_once("html.php");
-$name ="";
-$start = "nee";
-$test = "blabla";
 
-if(isset($_POST["delete"])){
-		$name = "";
-		$start = "nee";
-		$test = "omgjermlgjzemljmlkjmlrjgmlezrjg";
-		setcookie("name", "", strtotime("-1 year"));
-		if(isset($_SESSION['start'])){
-  			$_SESSION['start']= "nee";
-  			}
-	}
+$name = "";
+$start = "ja";
+$_POST['verwijder'] = "nee";
 
-if(isset($_POST["name"])){
-	setcookie("name" , ($_POST["name"]));
+
+if($_POST["name"] != ""){
+	setcookie("name" , ($_POST["name"]), strtotime('+1 year'));
+	session_start();
+	$start = "ja";
 	$_SESSION['start']= "ja";
-	$name = $_POST["name"];
+	$_POST["name"] = "";
 	}
 
 if(isset($_COOKIE["name"])){
- 		$name = $_COOKIE["name"];
+ 	$name = $_COOKIE["name"];
  	}
 
+if($_POST["verwijder"] == "delete"){
+	$name = "";
+	$start = "nee";
+	setcookie("name", "", strtotime("-1 year"));
+	if(isset($_SESSION['start'])){
+		session_destroy();
+  		$_SESSION['start']= "nee";
+  		}
+	}
+
 $content = new Div(
-				new Heading("$test").
-				new Heading("Session gestart? ==> ". $_SESSION['start']).
+				new Heading("Session gestart? ==> ". $start).
 				new Heading("Welkom ". $name).
 				new Form(
 				new Input("name", "text", array("class" => "form-control")) .
 				new Button("Login", array("class" => "btn btn-lg btn-primary btn-block", "type" => "submit")).
 				new Button("Delete",  array("class" => "btn btn-lg btn-primary btn-block", "type" => "submit"))
-				, array("class" => "form-signin", "method" => "post", "value" => "delete")
+				, array( "method" => "post", "name" => "verwijder", "value" => "delete")
 
-			), array("class" => "container")
+			), array("class" => "container", "method" => "post")
 		);	
 
  ?>
@@ -46,7 +49,6 @@ $content = new Div(
  	<title></title>
  </head>
  <body>
-
  	<?= $content ?>
  </body>
  </html>
